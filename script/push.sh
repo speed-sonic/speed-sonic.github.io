fbc265517a8fa6d6fd267581bc65fb9ebac893ae
speed-sonic <blldxt@gmail.com>
2020-02-19 21:18:22 +0800

    update the shell code

--- script/push.sh
+++ script/push.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 gitPush() {
-    commitText=$1;
-    echo "=======" $commitText "========";
     git add .;
-    git commit -m $commitText;
+    git commit -m "$1";
     git push origin master;
 }
 
 git status;
 
-echo -e "\n\nSubmit or not? (y|yes or other key)"
-read isCommit;
+echo -e "\n"
+read -p "Submit or not? (y|yes or other key): " isCommit;
 
 if [[ "$isCommit" =~ ^(y|yes)$ ]]; then
-    echo "Commit message:"
-    read commitText;
+    read -p "Commit message: " commitText;
     if [ -z "$commitText" ]; then
         commitText="update";
     fi
-    gitPush $commitText;
+    gitPush "$commitText";
 
-    echo -e "\n\ndone."
+    echo -e "\ndone."
 fi
